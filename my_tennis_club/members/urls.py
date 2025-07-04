c9013cf4b137788d239030b6481e2c49052b61a3
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-04 12:44:32 +0500

    updated

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    # path('', views.members, name='members'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
+    path('meri_tech/', views.meri_tech, name='meri_tech'),
 ]
  
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Members
 
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
 def members(request):
   mymembers = Members.objects.all().values()
   template = loader.get_template('all_members.html')
@@ -15,10 +19,21 @@ def details(request, id):
   template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
+
   }
   return HttpResponse(template.render(context, request))
   
-def main(request):
-  template = loader.get_template('main.html')
-  return HttpResponse(template.render())
-    
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry']
+  }
+  return HttpResponse(template.render(context, request))
+
+def meri_tech(request):
+  template = loader.get_template('myfirst.html')
+  context = {
+    'partner': ['Rao','Umair']
+  }
+  return HttpResponse(template.render(context, request))
